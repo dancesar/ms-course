881a7e1d9b1fa4eb47796dd543967cf2e8c0fdca
root <root@DESKTOP-FSLCI01.localdomain>
2021-02-13 02:58:24 -0300

    Config. Eureka Clients

--- hr-payroll/src/main/java/com/dancesar/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/dancesar/hrpayroll/HrPayrollApplication.java
@@ -2,13 +2,11 @@ package com.dancesar.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 
-@LoadBalancerClient(name = "hr-worker")
-//@RibbonClient(name = "hr-worker")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
